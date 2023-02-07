d3ec65d7e32807d3411062f2937dbc7f29e374c1
DsrKrasniii <donredd3rd@gmail.com>
2023-02-06 17:46:00 -0700

    hopefull this works

---
+++ WebApp/Models/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class Calculator
+    {
+        // Variables that are used in the get and set from the calculator app
+        public string var_1 { get; set; }
+        public string var_2 { get; set; }
+        public string var_3 { get; set; }
+        public string var_4 { get; set; }
+        public string var_5 { get; set; }
+    }
+}
