9b37f9cce8eed816636a996d65523e401f03f9b3
DsrKrasniii <donredd3rd@gmail.com>
2023-02-06 20:54:24 -0700

    finally

--- WebApp/Models/Calculator.cs
+++ WebApp/Models/Calculator.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApp.Models
 {
     public class Calculator
     {
         // Variables that are used in the get and set from the calculator app
-        public string var_1 { get; set; }
-        public string var_2 { get; set; }
-        public string var_3 { get; set; }
-        public string var_4 { get; set; }
-        public string var_5 { get; set; }
+        [Range(0, 100, ErrorMessage = "Values must be between 0 and 100")]
+        public int assignments { get; set; }
+        [Range(0, 100, ErrorMessage = "Values must be between 0 and 100")]
+        public int Group_Projects { get; set; }
+        [Range(0, 100, ErrorMessage = "Values must be between 0 and 100")]
+        public int Quizzes { get; set; }
+        [Range(0, 100, ErrorMessage = "Values must be between 0 and 100")]
+        public int Miderm_Exam { get; set; }
+        [Range(0, 100, ErrorMessage = "Values must be between 0 and 100")]
+        public int Final_Exam { get; set; }
+        [Range(0, 100, ErrorMessage = "Values must be between 0 and 100")]
+        public int Intex { get; set; }
     }
 }
